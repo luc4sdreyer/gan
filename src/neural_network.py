994c6044d1966f1d377a6480c1fa4e17c182bd63
Lucas Dreyer <luc4sdreyer@gmail.com>
2019-08-04 02:16:42 +0200

    Finished backpropagation

--- src/neural_network.py
+++ src/neural_network.py
@@ -23,8 +23,10 @@ class NeuralNetwork(object):
         inner_activation_function=activation_functions.logistic,
         outer_activation_function=activation_functions.logistic,
         initializer=lambda: random.gauss(0, 1),
-        num_iterations=1000):
+        num_iterations=1000,
+        learning_rate=0.1):
 
+        self.learning_rate = learning_rate
         self.num_iterations = num_iterations
         self.depth = len(layer_widths)
         self.inner_activation_function = inner_activation_function
@@ -79,6 +81,7 @@ class NeuralNetwork(object):
         return self.feedforward(result)
 
     def feedforward(self, input_list):
+        self._o[0] = input_list._arr.copy()[0]
         for i in range(self.depth -1):
             w = self.weights[i]
             input_list = input_list.multiply(w)
@@ -94,7 +97,7 @@ class NeuralNetwork(object):
 
             self._o[i+1] = input_list._arr.copy()[0]
 
-            print(input_list)
+            # print(input_list)
 
         return input_list._arr[0]
 
@@ -102,16 +105,16 @@ class NeuralNetwork(object):
         # get the error (delta), working backwards from the output layer
         delta = [None] * self.depth
         for current_depth in range(self.depth -1, 0, -1):
-            print("current_depth: %s" % current_depth)
+            # print("current_depth: %s" % current_depth)
             current_width = self.layer_widths[current_depth]
-            print("current_width: %s" % current_width)
+            # print("current_width: %s" % current_width)
             if current_depth == self.depth -1:
-                print("self._a: %s" % self._a)
+                # print("self._a: %s" % self._a)
                 delta_layer = Matrix([[self.outer_activation_function.df(self._a[current_depth][0])]])
             else:
                 activation_matrix = Matrix.create([current_width, current_width], lambda: 0.0)
                 for i in range(current_width):
-                    print("self._a[current_depth]: %s" % self._a[current_depth])
+                    # print("self._a[current_depth]: %s" % self._a[current_depth])
                     activation_matrix[i][i] = self.inner_activation_function.df(self._a[current_depth][i])
 
                 delta_layer = activation_matrix.multiply(self.weights[current_depth]).multiply(delta[current_depth + 1])
@@ -119,12 +122,38 @@ class NeuralNetwork(object):
             delta[current_depth] = delta_layer
         return delta
 
+    def _get_pdew(self, deltas):
+        """
+        Partial derivative of the error with respect to each weight (pdew).
+        """
+        # print("pdews")
+        # print(self._o)
+        # print(deltas)
+        pdews = [None] * self.depth
+        for i in range(0, self.depth -1):
+            pdews[i] = Matrix([self._o[i]]).transpose().multiply(deltas[i+1].transpose())
+        return pdews
+
     def train_step(self, X, y):
+        self.total_pdews = None
         for i in range(len(X)):
             actual_y = self.load(X[i])
             self.deltas = self._get_delta(X, y)
-            # partial derivative of the error (with respect to each weight)
-            # pdew = 
+            self.pdews = self._get_pdew(self.deltas)
+            if i == 0:
+                self.total_pdews = self.pdews
+            else:
+                for j in range(self.dept -1):
+                    self.total_pdews[j].add(self.pdews[j])
+
+        for j in range(0, self.depth -1):
+            self.total_pdews[j].multiply_scalar(1.0 / len(X) * -self.learning_rate)
+
+        self.weight_adjustments = self.total_pdews
+
+        for j in range(0, self.depth -1):
+            self.weights[j].add(self.weight_adjustments[j])
+
 
     def train(self, X, y):
         for epoch in range(self.num_iterations):
